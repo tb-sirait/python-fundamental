4b14db8a7a123a551a0fa7f1b1e62c48a3a0acb5
Taor Baga <79407412+tb-sirait@users.noreply.github.com>
2022-12-14 21:41:06 +0700

    anu

---
+++ Days 1/jawaban/helloworld.py
@@ -0,0 +1,47 @@
+# Day 1
+
+# Level 1
+operand1 = 5
+operand2 = 6
+
+div = '/'
+add = '+'
+sub = '-'
+mlt = '*'
+mod = '%'
+exp = "**"
+fld = "//"
+
+addition = operand1+operand2
+substraction = operand1-operand2
+multiplication = operand1*operand2
+modulus = operand1%operand2
+division = operand1/operand2
+exponential = operand1**operand2
+floordivision = operand1//operand2
+
+print("LVL 1 -2")
+print("Hasil dari " , operand1 , add , operand2 , "= " , addition)
+print("Hasil dari " , operand1 , sub , operand2 , "= " , substraction)
+print("Hasil dari " , operand1 , mlt , operand2 , "= " , multiplication)
+print("Hasil dari " , operand1 , mod , operand2 , "= " , modulus)
+print("Hasil dari " , operand1 , div , operand2 , "= " , division)
+print("Hasil dari " , operand1 , exp , operand2 , "= " , exponential)
+print("Hasil dari " , operand1 , fld , operand2 , "= " , floordivision)
+print("")
+
+print("LVL 1 -3")
+inputs = input("NAMA: ")
+inputs2 = input("NIM: ")
+inputs3 = input("ASAL DAERAH: ")
+print("Nama mu ", inputs, " dengan NIM ", inputs2, " asal daerahmu ", inputs3)
+
+print("LVL 1 -4")
+print(type(12))
+print(type(7.4))
+print(type(3.14))
+print(type(5-6j))
+print(type(['budi', '105201001', 'jekardah']))
+print(type("Taor"))
+print(type(105220031))
+print(type("Jekardah"))
