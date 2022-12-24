d893a4c58c6c1b49b55cb0d3fd8264ed4b027459
Taor Baga <79407412+tb-sirait@users.noreply.github.com>
2022-12-24 08:12:21 +0700

    lvl1

---
+++ Days 3/jawaban/taor-baga_day3-lvl1.py
@@ -0,0 +1,30 @@
+umur = int(input("masukkan umur anda: "))
+print(umur)
+tinggi = float(input("masukkan tinggi badan anda: "))
+print(tinggi)
+print(complex(7.5, 9))
+
+alas = int(input("Masukkan Alas: "))
+tinggi = int(input("Masukkan Tinggi: "))
+luas_segitiga = 0.5*alas*tinggi
+print("Luas Segitiga adalah ", int(luas_segitiga))
+
+a = int(input("masukkan sisi a: "))
+b = int(input("masukkan sisi b: "))
+c = int(input("masukkan sisi c: "))
+keliling_segitiga = a+b+c
+print("keliling Segitiga adalah ",keliling_segitiga)
+
+pjg = int(input("Masukkan Panjang: "))
+lbr = int(input("Masukkan Lebar: "))
+keliling_persegi_panjang = 2*(pjg+lbr)
+luas_persegi_panjang = pjg * lbr
+print("keliling persegi panjang adalah ",keliling_persegi_panjang)
+print("Luas persegi panjang adalah ",luas_persegi_panjang)
+
+pi = 3.14
+r = int(input("Masukkan Jari Jari"))
+luas_lingkaran = pi*r*r
+keliling_lingkaran = 2*pi*r
+print("Luas lingkaran adalah ", float(luas_lingkaran))
+print("keliling lingkaran adalah ", float(keliling_lingkaran))
